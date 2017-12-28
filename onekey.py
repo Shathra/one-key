dd496a8db0397eb86f70e7f6246f93ec4610ba47
Selçuk Gülcan <selcuk.gulcan@bilkent.edu.tr>
2017-12-28 21:16:07 +0300

    added OneKey class and a main function

--- onekey.py
+++ onekey.py
@@ -1,5 +1,147 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+import json
+import os
+import getpass
+
+_DEFAULT_PATH = "lemonade.psx"
+
+
+class OneKey:
+
+    def __init__(self, master_key, path=_DEFAULT_PATH):
+        self.path = path
+        self.key_dict = None
+        self.master_key = master_key
+
+    def _export_file(self):
+        def pad(s):
+            s = s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
+            return s
+
+        iv = Random.new().read(AES.block_size)
+        aes = AES.new(self.master_key, AES.MODE_CBC, iv)
+        plain_text = json.dumps(self.key_dict)
+        plain_text = pad(plain_text)
+        cipher_text = aes.encrypt(plain_text)
+        content = iv + cipher_text
+        file = open(self.path, "wb")
+        file.write(content)
+        file.close()
+
+    def import_file(self):
+        """
+        Import an encrypted file with a given key, deserializes it into a dict.
+        """
+        def unpad(s):
+            s = s[:-ord(s[len(s) - 1:])]
+            return s
+
+        file = open(self.path, "rb")
+        content = file.read()
+        file.close()
+
+        iv = content[:AES.block_size]
+        cipher_text = content[AES.block_size:]
+
+        aes = AES.new(self.master_key, AES.MODE_CBC, iv)
+        plain_text = aes.decrypt(cipher_text)
+        plain_text = unpad(plain_text)
+        plain_text = plain_text.decode("utf-8")
+        self.key_dict = json.loads(plain_text)
+
+    def command_list(self, arg):
+        """
+        Returns a list of stored key values.
+        :param arg: If none all keys are retrieved, otherwise only keys containing arg will be returned.
+        :return: list of keys
+        """
+        if arg is None:
+            retval = [x for x in self.key_dict.keys()]
+
+        else:
+            retval = [x for x in self.key_dict.keys() if arg.lower() in x]
+
+        return retval
+
+    def command_add(self, key, value):
+        """
+        Add a value with a given key, and store it
+        :param key: key to add
+        :param value: Corresponding value
+        """
+        self.key_dict[key] = value
+
+        self._export_file()
+
+    def command_del(self, key):
+        """
+        Deletes a key and store changes.
+        :param key: key to delete
+        """
+        self.key_dict.pop(key, None)
+
+        self._export_file()
+
+    def command_view(self, key):
+        """
+        Returns value with given key
+        :param key: key
+        :return: value
+        """
+        retval = None
+
+        if key in self.key_dict.keys():
+            retval = self.key_dict[key]
+
+        return retval
+
+
 def main():
-    pass
+
+    key = getpass.getpass("Master Key:")
+    ok = OneKey(key)
+
+    is_file_exist = os.path.isfile(_DEFAULT_PATH)
+    if not is_file_exist:
+        ok.key_dict = dict()
+
+    else:
+        ok.import_file()
+
+    command = None
+    while command != "quit":
+
+        input_arr = input().split()
+        command = input_arr[0]
+        arg = None
+        if len(input_arr) > 1:
+            arg = input_arr[1]
+
+        if command == "list":
+            key_list = ok.command_list(arg)
+            print()
+            for key in key_list:
+                print(key)
+
+        elif command == "add":
+            if len(input_arr) > 1:
+                key = input_arr[1]
+                value = getpass.getpass()
+                ok.command_add(key, value)
+
+        elif command == "view":
+            if len(input_arr) > 1:
+                key = input_arr[1]
+                value = ok.command_view(key)
+                print()
+                print(value)
+
+        elif command == "clear":
+            os.system('cls||clear')
+
+        elif command == "del":
+            ok.command_del(arg)
 
 
 if __name__ == "__main__":
--- test_onekey.py
+++ test_onekey.py
@@ -1,10 +1,94 @@
 import unittest
+from onekey import OneKey
+import shutil
+import os
+
+filename = "test.psx"
+key = "keymust16bytelon"
 
 
 class OneKeyUnitTest(unittest.TestCase):
 
+    def setUp(self):
+        shutil.copy("test.ts", filename)
+
+    def tearDown(self):
+        os.remove(filename)
+
     def test_travis(self):
         self.assertEqual(True, True)
+
+    def test_import_file(self):
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        key_dict = onekey.key_dict
+
+        self.assertEqual("value", key_dict["key"])
+
+    def test_command_list(self):
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        c_list = onekey.command_list(None)
+        no = len(c_list)
+        self.assertEqual(1, no)
+
+        self.assertIn("key", c_list)
+
+        c_list = onekey.command_list("k")
+        no = len(c_list)
+        self.assertEqual(1, no)
+
+        self.assertIn("key", c_list)
+
+        c_list = onekey.command_list("a")
+        no = len(c_list)
+        self.assertEqual(0, no)
+
+    def test_command_add(self):
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        onekey.command_add("gandalf", "You shall not pass")
+
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+        self.assertEqual(onekey.key_dict["gandalf"], "You shall not pass")
+        self.assertEqual(onekey.key_dict["key"], "value")
+
+    def test_command_del(self):
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        onekey.command_del("key")
+
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        count = len(onekey.key_dict.keys())
+        self.assertEqual(0, count)
+
+    def test_view(self):
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        value = onekey.command_view("ke")
+        self.assertEqual(None, value)
+
+        value = onekey.command_view("key")
+        self.assertEqual("value", value)
+
+    def test_add_and_view(self):
+        onekey = OneKey(key, path=filename)
+        onekey.import_file()
+
+        onekey.command_add("hi", "hello")
+        onekey_test = OneKey(key, path=filename)
+        onekey_test.import_file()
+
+        value = onekey.command_view("hi")
+        self.assertEqual("hello", value)
 
 
 if __name__ == '__main__':
